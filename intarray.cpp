5db85a53ec172c22e09371dc53b4a31d1b9818b8
root <vedantam@chapman.edu>
2020-01-31 03:02:57 +0000

    updated

--- intarray.cpp
+++ intarray.cpp
@@ -7,12 +7,23 @@ int main(int argc, char **argv){
 
   cout << "Enter ten non-negative numbers" << endl;
   for(int i = 0; i < 10; ++ i){
-    cin >> numberArray[i];
+    int number;
+    cin >> number;
+    if(number >= 0){
+      numberArray[i] = number;
+      //cin >> numberArray[i];
+    }
+    else{
+      cout << "this is a negative number. Please enter a positive number." <<endl;
+      cin >> number;
+      numberArray[i] = number;
+    }
+
   }
 // adds user input into an array ^
   for(int i = 0; i < 10; ++i){
     cout << numberArray[i];
   }
-// outputs the array 
+// outputs the array
   cout << endl;
 }
